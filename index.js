dd5dbd9321f5af540eac7fa859857a4296576723
mekagojiradev <lightswornnarwhal@gmail.com>
2024-10-17 13:23:35 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.js"></script>
+    <script src="script.js"></script> <!-- Link to your external script.js file -->
+</head>
+<body>
+</body>
+</html>
